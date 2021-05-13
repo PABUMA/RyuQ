67973371f9b9ffe39c37958c2c31fa08a03478c8
ajnebro <ajnebro@users.noreply.github.com>
2021-05-13 13:24:12 +0200

    Improve auto-documentation

--- src/main/java/org/pabuma/ryuq/simulatedannealing/cooling/impl/Geometric.java
+++ src/main/java/org/pabuma/ryuq/simulatedannealing/cooling/impl/Geometric.java
@@ -1,9 +1,13 @@
 package org.pabuma.ryuq.simulatedannealing.cooling.impl;
 
+import org.pabuma.ryuq.simulatedannealing.SimulatedAnnealing;
 import org.pabuma.ryuq.simulatedannealing.cooling.CoolingScheme;
 
+/**
+ * Geometric cooling scheme used by {@link SimulatedAnnealing} algorithms
+ */
 public class Geometric implements CoolingScheme {
-  private double alpha ;
+  private final double alpha ;
 
   public Geometric(double alpha) {
     this.alpha = alpha ;
--- src/main/java/org/pabuma/ryuq/simulatedannealing/cooling/impl/Linear.java
+++ src/main/java/org/pabuma/ryuq/simulatedannealing/cooling/impl/Linear.java
@@ -1,9 +1,13 @@
 package org.pabuma.ryuq.simulatedannealing.cooling.impl;
 
+import org.pabuma.ryuq.simulatedannealing.SimulatedAnnealing;
 import org.pabuma.ryuq.simulatedannealing.cooling.CoolingScheme;
 
+/**
+ * Linear cooling scheme used by {@link SimulatedAnnealing} algorithms
+ */
 public class Linear implements CoolingScheme {
-  private double beta ;
+  private final double beta ;
 
   public Linear(double beta) {
     this.beta = beta ;
