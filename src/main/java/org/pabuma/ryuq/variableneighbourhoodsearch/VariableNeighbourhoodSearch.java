7518fc79186d0d1d9d71bc8877d51de0aa97219c
Oleg <brezitskyyoleg@gmail.com>
2021-05-15 15:11:50 +0200

    First commit of VNS algorithm

---
+++ src/main/java/org/pabuma/ryuq/variableneighbourhoodsearch/VariableNeighbourhoodSearch.java
@@ -0,0 +1,28 @@
+package org.pabuma.ryuq.variableneighbourhoodsearch;
+
+import org.pabuma.ryuq.TrajectoryAlgorithm;
+import org.pabuma.ryuq.component.createinitialsolution.CreateInitialSolution;
+import org.pabuma.ryuq.component.terminationcondition.TerminationCondition;
+import org.uma.jmetal.problem.Problem;
+import org.uma.jmetal.solution.Solution;
+
+/**
+ * Class implementing a Variable Neighbourhood Search algorithm by extending the {@link
+ * org.pabuma.ryuq.TrajectoryAlgorithm} interface.
+ *
+ * @author Carmen Lucía Arrabalí Cañete, Talal Awija, Oleg Brezitskyy, Sergio Martín Vera
+ * @param <S> Generic type of the problem solutions
+ */
+public class VariableNeighbourhoodSearch<S extends Solution<?>> extends TrajectoryAlgorithm<S> {
+  public VariableNeighbourhoodSearch(
+      Problem<S> problem,
+      CreateInitialSolution<S> createInitialSolution,
+      TerminationCondition terminationCondition) {
+    super(problem, createInitialSolution, terminationCondition);
+  }
+
+  @Override
+  public S upgrade(S currentSolution) {
+    return null;
+  }
+}
