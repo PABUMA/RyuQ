5ce3c228e9371bef9d87050badaf30c16bf93585
InmaJimenez26 <inmajimenezpalomino@uma.es>
2021-05-18 12:37:03 +0200

    Añadida clase principal

--- src/main/java/org/pabuma/ryuq/iteratedlocalsearch/IteratedLocalSearch.java
+++ src/main/java/org/pabuma/ryuq/iteratedlocalsearch/IteratedLocalSearch.java
@@ -1,4 +1,32 @@
 package org.pabuma.ryuq.iteratedlocalsearch;
 
-public class IteratedLocalSearch {
+import org.pabuma.ryuq.TrajectoryAlgorithm;
+import org.pabuma.ryuq.component.createinitialsolution.CreateInitialSolution;
+import org.pabuma.ryuq.component.terminationcondition.TerminationCondition;
+import org.uma.jmetal.operator.mutation.MutationOperator;
+import org.uma.jmetal.problem.Problem;
+import org.uma.jmetal.solution.Solution;
+
+public class IteratedLocalSearch<S extends Solution<?>> extends TrajectoryAlgorithm<S> {
+
+    public IteratedLocalSearch(Problem<S> problem, CreateInitialSolution<S> createInitialSolution,
+                               TerminationCondition terminationCriterion){
+        super(problem, createInitialSolution, terminationCriterion);
+
+    }
+
+    @Override
+    public S upgrade(S currentSolution) {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return "VNS";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Variable Neighbourhood Search";
+    }
 }
