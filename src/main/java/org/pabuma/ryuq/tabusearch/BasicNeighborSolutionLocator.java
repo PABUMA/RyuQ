1c8514d084bbdac84420e2c64bd03d1c9e2513ee
paulandujar <paulandujar@uma.es>
2021-05-18 20:16:29 +0200

    clases

--- src/main/java/org/pabuma/ryuq/tabusearch/BasicNeighborSolutionLocator.java
+++ src/main/java/org/pabuma/ryuq/tabusearch/BasicNeighborSolutionLocator.java
@@ -1,2 +1,37 @@
-package org.pabuma.ryuq.tabusearch;public class BasicNeighborSolutionLocator {
+package org.pabuma.ryuq.tabusearch;
+
+public class BasicNeighborSolutionLocator {
+    import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.collections4.Predicate;
+
+   
+    public class BasicNeighborSolutionLocator implements BestNeighborSolutionLocator {
+
+
+        @Override
+        public Solution findBestNeighbor(List<Solution> neighborsSolutions, final List<Solution> solutionsInTabu) {
+ 
+            CollectionUtils.filterInverse(neighborsSolutions, new Predicate<Solution>() {
+                @Override
+                public boolean evaluate(Solution neighbor) {
+                    return solutionsInTabu.contains(neighbor);
+                }
+            });
+
+
+            Collections.sort(neighborsSolutions, new Comparator<Solution>() {
+                @Override
+                public int compare(Solution a, Solution b) {
+                    return a.getValue().compareTo(b.getValue());
+                }
+            });
+            
+            return neighborsSolutions.get(0);
+        }
+
+    }
 }
--- src/main/java/org/pabuma/ryuq/tabusearch/IterationsStopConditions.java
+++ src/main/java/org/pabuma/ryuq/tabusearch/IterationsStopConditions.java
@@ -1,2 +1,18 @@
-package org.pabuma.ryuq.tabusearch;public class IterationsStopConditions {
+package org.pabuma.ryuq.tabusearch;
+
+public class IterationsStopConditions {
+    public class IterationsStopCondition implements StopCondition {
+
+        private final Integer maxIterations;
+
+        public IterationsStopCondition(Integer maxIterations) {
+            this.maxIterations = maxIterations;
+        }
+
+        @Override
+        public Boolean mustStop(Integer currentIteration, Solution bestSolutionFound) {
+            return currentIteration >= maxIterations;
+        }
+
+    }
 }
