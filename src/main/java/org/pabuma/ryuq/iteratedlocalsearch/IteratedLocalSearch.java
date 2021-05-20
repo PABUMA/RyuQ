b9e0d7cc6a228398831f100910b9bcf46bc8136a
Inés Díaz <inesdr6@uma.es>
2021-05-20 18:14:39 +0200

    Updated class

--- src/main/java/org/pabuma/ryuq/iteratedlocalsearch/IteratedLocalSearch.java
+++ src/main/java/org/pabuma/ryuq/iteratedlocalsearch/IteratedLocalSearch.java
@@ -7,16 +7,41 @@ import org.uma.jmetal.operator.mutation.MutationOperator;
 import org.uma.jmetal.problem.Problem;
 import org.uma.jmetal.solution.Solution;
 
+import java.util.ArrayList;
+
 public class IteratedLocalSearch<S extends Solution<?>> extends TrajectoryAlgorithm<S> {
 
-    public IteratedLocalSearch(Problem<S> problem, CreateInitialSolution<S> createInitialSolution,
+    private ArrayList<S> history;
+    private S localSearchSolution;
+
+    public IteratedLocalSearch(Problem<S> problem,
+                               CreateInitialSolution<S> createInitialSolution,
                                TerminationCondition terminationCriterion){
         super(problem, createInitialSolution, terminationCriterion);
-
+        this.localSearchSolution = LocalSearch(createInitialSolution.create());
     }
 
     @Override
     public S upgrade(S currentSolution) {
+        // currentSolution = localSearchSolution;
+        S locMinSolution = perturbation(currentSolution, history);
+        problem.evaluate(locMinSolution);
+        S localSearchSolution2 = LocalSearch(locMinSolution);
+        if (localSearchSolution2.objectives()[0] < currentSolution.objectives()[0]){
+            currentSolution = localSearchSolution2;
+        }
+        return currentSolution;
+    }
+
+    public S LocalSearch(S currentSolution){
+       /* if (x < currentSolution.objectives()[0]) {
+        *   currentSolution = x;
+        * }
+        */
+        return currentSolution ;
+    }
+
+    public S perturbation(S currentSolution, ArrayList<S> history){
         return null;
     }
 
