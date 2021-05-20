9e1fb48e599a7b9c197860c5f73359111e55d49b
ajnebro <ajnebro@users.noreply.github.com>
2021-05-20 13:12:07 +0200

    Add algorithm Local Search

---
+++ src/main/java/org/pabuma/ryuq/localsearch/examples/LocalSearchDoubleExample.java
@@ -0,0 +1,31 @@
+package org.pabuma.ryuq.localsearch.examples;
+
+import org.pabuma.ryuq.component.createinitialsolution.impl.DefaultSolutionCreation;
+import org.pabuma.ryuq.component.terminationcondition.impl.TerminationByEvaluations;
+import org.pabuma.ryuq.localsearch.LocalSearch;
+import org.pabuma.ryuq.simulatedannealing.SimulatedAnnealing;
+import org.pabuma.ryuq.simulatedannealing.cooling.impl.Geometric;
+import org.uma.jmetal.operator.mutation.MutationOperator;
+import org.uma.jmetal.operator.mutation.impl.PolynomialMutation;
+import org.uma.jmetal.problem.doubleproblem.DoubleProblem;
+import org.uma.jmetal.problem.singleobjective.Rastrigin;
+import org.uma.jmetal.solution.doublesolution.DoubleSolution;
+import org.uma.jmetal.util.observer.impl.PrintObjectivesObserver;
+
+public class LocalSearchDoubleExample {
+  public static void main(String[] args) {
+    DoubleProblem problem = new Rastrigin(20) ;
+    MutationOperator<DoubleSolution> mutation = new PolynomialMutation(1.0/problem.getNumberOfVariables(), 20.0) ;
+
+    LocalSearch<DoubleSolution> localSearch = new LocalSearch<>(
+            problem, mutation, new DefaultSolutionCreation<>(problem), new TerminationByEvaluations(500000)) ;
+
+    PrintObjectivesObserver objectivesObserver = new PrintObjectivesObserver(10000) ;
+    localSearch.getObservable().register(objectivesObserver);
+
+    localSearch.run();
+
+    System.out.println("Best solution: " + localSearch.getResult().objectives()[0]) ;
+    System.out.println("Computing tine: " + localSearch.getTotalComputingTime()) ;
+  }
+}
