ad4ec0c72a45298be236e977104b9c11dddb7341
Inés Díaz <inesdr6@uma.es>
2021-05-26 09:14:00 +0200

    Added MSA

---
+++ src/main/java/org/pabuma/ryuq/msa/MSASolution.java
@@ -0,0 +1,78 @@
+package org.pabuma.ryuq.msa;
+
+import org.uma.jmetal.solution.AbstractSolution;
+import org.uma.jmetal.solution.Solution;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.IntStream;
+
+/**
+ * Class representing a multiple sequence alignment (MSA). Each sequence is stored as a {@link StringBuilder}
+ * object. It is assumed that the MSA is already aligned.
+ */
+public class MSASolution extends AbstractSolution<StringBuilder> {
+
+    /**
+     * Constructor
+     * @param numberOfVariables
+     * @param numberOfObjectives
+     */
+    public MSASolution(int numberOfVariables, int numberOfObjectives) {
+        super(numberOfVariables, numberOfObjectives);
+    }
+
+    public MSASolution(List<StringBuilder> sequences, int numberOfScores) {
+        super(sequences.size(), numberOfScores) ;
+
+        for (int i = 0; i < sequences.size(); i++) {
+            variables().set(i, new StringBuilder(sequences.get(i))) ;
+        }
+    }
+
+    /**
+     * Copy constructor
+     * @return
+     */
+
+    public MSASolution(MSASolution solution) {
+        this(solution.variables().size(), solution.objectives().length) ;
+
+        for (int i = 0; i < variables().size(); i++) {
+            variables().set(i, new StringBuilder(solution.variables().get(i)));
+        }
+
+        for (int i = 0; i < objectives().length; i++) {
+            objectives()[i] = solution.objectives()[i];
+        }
+
+        for (int i = 0; i < constraints().length; i++) {
+            constraints()[i] =  solution.constraints()[i];
+        }
+
+        attributes = new HashMap<>(solution.attributes);
+    }
+
+    @Override
+    public Solution copy() {
+        return new MSASolution(this);
+    }
+
+    public void removeGapColumns() {
+        for (int i = 0; i < variables().get(0).length(); i++) {
+            if (columnHasOnlyGaps(i)) {
+                removeColumn(i) ;
+            }
+        }
+    }
+
+    private boolean columnHasOnlyGaps(int index) {
+        int numberColumnsWithGaps = (int) variables().stream().filter(sequence -> sequence.charAt(index) == '-').count();
+
+        return numberColumnsWithGaps == variables().size() ;
+    }
+
+    private void removeColumn(int index) {
+        variables().forEach(sequence -> sequence.deleteCharAt(index));
+    }
+}
---
+++ src/main/java/org/pabuma/ryuq/msa/mutation/RandomGapInsertion.java
@@ -0,0 +1,40 @@
+package org.pabuma.ryuq.msa.mutation;
+
+import org.pabuma.ryuq.msa.MSASolution;
+import org.uma.jmetal.operator.mutation.MutationOperator;
+import org.uma.jmetal.util.pseudorandom.JMetalRandom;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RandomGapInsertion implements MutationOperator<MSASolution> {
+    private final double mutationProbability;
+
+    public RandomGapInsertion(double mutationProbability) {
+        if ((mutationProbability < 0.0) || (mutationProbability > 1.0)) {
+            throw new RuntimeException("Invalid mutation probability value: " + mutationProbability);
+        }
+        this.mutationProbability = mutationProbability;
+    }
+
+    @Override
+    public double getMutationProbability() {
+        return mutationProbability;
+    }
+
+    @Override
+    public MSASolution execute(MSASolution solution) {
+        if (null == solution) {
+            throw new RuntimeException("Null solution");
+        }
+
+        if (JMetalRandom.getInstance().nextDouble() < mutationProbability) {
+            solution.variables().forEach(sequence -> {
+                int newGapPosition = JMetalRandom.getInstance().nextInt(0, sequence.length());
+                sequence.insert(newGapPosition, "-");
+            });
+        }
+
+        return solution;
+    }
+}
